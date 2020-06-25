062fc2fd8fac7bf70fb281a37e280bfc68e2fb7b
Caio Rodrigues <caio.rm1@gmail.com>
2020-06-25 11:59:15 -0300

    update version number

--- PanamahSDK.Consts.pas
+++ PanamahSDK.Consts.pas
@@ -3,12 +3,12 @@ unit PanamahSDK.Consts;
 interface
 
 const
-  SDK_VERSION = '1.0.1';
+  SDK_VERSION = '1.0.2';
   UTF8_CODEPAGE = 65001;
   LATIN_CODEPAGE = 28591;
   HTTPS_PROTOCOL = 'https';
   API_BASE_URL = 'https://panamah.io/api/v2';
-  SDK_IDENTITY = 'panamah-delphi1.0.0';
+  SDK_IDENTITY = 'panamah-delphi1.0.2';
 
 implementation
 
